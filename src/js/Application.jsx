5ae13a4ff1466ddf7a47117685add580abc216f3
Lukas Gisder-Dubé <gisderdube@gmail.com>
2018-09-11 11:05:06 +0200

    pass click function as props

--- src/js/Application.jsx
+++ src/js/Application.jsx
@@ -5,42 +5,55 @@ class Application extends React.Component {
     constructor(props) {
         super(props)
 
-        this.state = {}
+        this.state = {
+            cards: [
+                {
+                    img: 'https://picsum.photos/300/300',
+                    heading: 'Declarative',
+                    caption:
+                        'Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+                },
+                {
+                    img: 'https://picsum.photos/400/400',
+                    heading: 'Components',
+                    caption:
+                        'Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+                },
+                {
+                    img: 'https://picsum.photos/500/500',
+                    heading: 'Single-Way',
+                    caption:
+                        'Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+                },
+                {
+                    img: 'https://picsum.photos/600/600',
+                    heading: 'JSX',
+                    caption:
+                        'Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+                },
+            ],
+        }
+
+        this._changePicture = this._changePicture.bind(this)
     }
 
     render() {
-        const cards = [
-            {
-                img: 'https://picsum.photos/300/300',
-                heading: 'Declarative',
-                caption:
-                    'Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
-            },
-            {
-                img: 'https://picsum.photos/400/400',
-                heading: 'Components',
-                caption:
-                    'Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
-            },
-            {
-                img: 'https://picsum.photos/500/500',
-                heading: 'Single-Way',
-                caption:
-                    'Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
-            },
-            {
-                img: 'https://picsum.photos/600/600',
-                heading: 'JSX',
-                caption:
-                    'Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
-            },
-        ]
-
-        const mappedCards = cards.map((card, index) => (
-            <Card img={card.img} heading={card.heading} caption={card.caption} key={index} />
+        const mappedCards = this.state.cards.map((card, index) => (
+            <Card
+                img={card.img}
+                heading={card.heading}
+                caption={card.caption}
+                index={index}
+                key={index}
+                changePicture={this._changePicture}
+            />
         ))
 
         return <div className="container card-flex">{mappedCards}</div>
+    }
+
+    _changePicture(index) {
+        console.log('HELLO', index)
     }
 }
 
--- src/js/Card.jsx
+++ src/js/Card.jsx
@@ -6,6 +6,7 @@ const Card = props => {
             <img src={props.img} alt="" />
             <h3>{props.heading}</h3>
             <p>{props.caption}</p>
+            <button onClick={() => props.changePicture(props.index)}>Click to change the picture</button>
         </div>
     )
 }
