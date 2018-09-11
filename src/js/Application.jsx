8a5882b897e0cf41113a2365ea7b8d48200e6b65
Lukas Gisder-Dubé <gisderdube@gmail.com>
2018-09-11 12:29:03 +0200

    change border colors of cards

--- src/js/Application.jsx
+++ src/js/Application.jsx
@@ -12,29 +12,36 @@ class Application extends React.Component {
                     heading: 'Declarative',
                     caption:
                         'Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+                    borderColor: this._generateRandomColor(),
                 },
                 {
                     img: 'https://picsum.photos/400/400',
                     heading: 'Components',
                     caption:
                         'Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+                    borderColor: this._generateRandomColor(),
                 },
                 {
                     img: 'https://picsum.photos/500/500',
                     heading: 'Single-Way',
                     caption:
                         'Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+                    borderColor: this._generateRandomColor(),
                 },
                 {
                     img: 'https://picsum.photos/600/600',
                     heading: 'JSX',
                     caption:
                         'Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+                    borderColor: this._generateRandomColor(),
                 },
             ],
         }
 
+        console.log(this.state)
+
         this._changePicture = this._changePicture.bind(this)
+        this._changeBorder = this._changeBorder.bind(this)
     }
 
     render() {
@@ -43,9 +50,11 @@ class Application extends React.Component {
                 img={card.img}
                 heading={card.heading}
                 caption={card.caption}
+                borderColor={card.borderColor}
                 index={index}
                 key={index}
                 changePicture={this._changePicture}
+                changeBorder={this._changeBorder}
             />
         ))
 
@@ -59,6 +68,26 @@ class Application extends React.Component {
                 else return { ...card, img: 'https://picsum.photos/700/700' }
             }),
         })
+    }
+
+    _changeBorder(index) {
+        this.setState({
+            cards: this.state.cards.map((card, i) => {
+                if (i !== index) return card
+                else return { ...card, borderColor: this._generateRandomColor() }
+            }),
+        })
+    }
+
+    _generateRandomColor() {
+        return `#${this._generateRandomInt(0, 255).toString(16)}${this._generateRandomInt(
+            0,
+            255
+        ).toString(16)}${this._generateRandomInt(0, 255).toString(16)}`
+    }
+
+    _generateRandomInt(min, max) {
+        return Math.floor(Math.random() * Math.floor(max - min)) + min
     }
 }
 
--- src/js/Card.jsx
+++ src/js/Card.jsx
@@ -2,11 +2,15 @@ import React, { Component } from 'react'
 
 const Card = props => {
     return (
-        <div className="card">
+        <div
+            className="card"
+            style={{ borderColor: props.borderColor, borderWidth: 5, borderStyle: 'solid' }}
+        >
             <img src={props.img} alt="" />
             <h3>{props.heading}</h3>
             <p>{props.caption}</p>
-            <button onClick={() => props.changePicture(props.index)}>Click to change the picture</button>
+            <button onClick={() => props.changePicture(props.index)}>change the picture</button>
+            <button onClick={() => props.changeBorder(props.index)}>change the border</button>
         </div>
     )
 }
