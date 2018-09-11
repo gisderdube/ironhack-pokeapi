1da6bc4f506f9ee25c16f7099f5b3e9ecd435400
Lukas Gisder-Dubé <gisderdube@gmail.com>
2018-09-11 10:36:46 +0200

    transformed Card.jsx into a functional component

--- src/js/Application.jsx
+++ src/js/Application.jsx
@@ -12,6 +12,7 @@ class Application extends React.Component {
         return (
             <div className="container">
                 <Card img='https://picsum.photos/300/300' heading='Declarative' caption='Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.' />
+                <Card img='https://picsum.photos/400/400' heading='Components' caption='Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.' />
             </div>
         )
     }
--- src/js/Card.jsx
+++ src/js/Card.jsx
@@ -1,15 +1,13 @@
 import React, { Component } from 'react'
 
-class Card extends Component {
-    render() {
-        return (
-            <div className="card">
-                <img src={this.props.img} alt="" />
-                <h3>{this.props.heading}</h3>
-                <p>{this.props.caption}</p>
-            </div>
-        )
-    }
+const Card = props => {
+    return (
+        <div className="card">
+            <img src={props.img} alt="" />
+            <h3>{props.heading}</h3>
+            <p>{props.caption}</p>
+        </div>
+    )
 }
 
 export default Card
