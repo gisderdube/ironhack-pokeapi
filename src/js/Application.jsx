264daf27eb20019cd3327ee840c3ea8484d6dd70
Lukas Gisder-Dubé <gisderdube@gmail.com>
2018-09-11 10:54:25 +0200

    basic mapping

--- src/js/Application.jsx
+++ src/js/Application.jsx
@@ -9,12 +9,38 @@ class Application extends React.Component {
     }
 
     render() {
-        return (
-            <div className="container">
-                <Card img='https://picsum.photos/300/300' heading='Declarative' caption='Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.' />
-                <Card img='https://picsum.photos/400/400' heading='Components' caption='Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.' />
-            </div>
-        )
+        const cards = [
+            {
+                img: 'https://picsum.photos/300/300',
+                heading: 'Declarative',
+                caption:
+                    'Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+            },
+            {
+                img: 'https://picsum.photos/400/400',
+                heading: 'Components',
+                caption:
+                    'Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+            },
+            {
+                img: 'https://picsum.photos/500/500',
+                heading: 'Single-Way',
+                caption:
+                    'Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+            },
+            {
+                img: 'https://picsum.photos/600/600',
+                heading: 'JSX',
+                caption:
+                    'Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+            },
+        ]
+
+        const mappedCards = cards.map((card, index) => (
+            <Card img={card.img} heading={card.heading} caption={card.caption} key={index} />
+        ))
+
+        return <div className="container card-flex">{mappedCards}</div>
     }
 }
 
